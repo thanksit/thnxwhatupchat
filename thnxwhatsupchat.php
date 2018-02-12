51cf8ed6f72306fbfd7e7b1a7aec5297d664f658
= <=>
2018-02-13 03:39:00 +0600

    intial commit

--- thnxwhatsupchat.php
+++ thnxwhatsupchat.php
@@ -171,8 +171,8 @@ class Thnxwhatsupchat extends Module
         $helper->table =  $this->table;
         $lang = new Language((int)Configuration::get('PS_LANG_DEFAULT'));
         $helper->default_form_language = $lang->id;
-        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') 
-        ? Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') : 0;
+        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG')
+         ? Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') : 0;
         $this->fields_form = array();
 
         $helper->identifier = $this->identifier;
